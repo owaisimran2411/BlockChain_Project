86130a5f9ea4d4d4109f2ee37a2fef65c77d09f6
owaisimran2411 <56302075+owaisimran2411@users.noreply.github.com>
2022-05-27 11:50:59 +0500

    Final Project Upload

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "079ba87a3c7f49fa8e3e5a640a858beb";
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: "0.8.4",
+};
